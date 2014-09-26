86f14b718c58c5ea6903b8320f8612065d3cd6c1
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2014-09-26 17:16:38 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SelectMyMajor::Application.config.secret_token = 'dd67a7de5d47a648068aa993cac53fda3c47117862d9a4352764678e4b3d4d2f0715ab3350359732ff0a03c5063149f9686aef6905c204c569ed68cc7f8fe3e0'

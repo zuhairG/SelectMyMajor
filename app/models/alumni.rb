4ec24a8e165ccb0d1695485eefe5658a6362fc2d
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2014-09-26 19:23:54 -0400

    association syntax fixed

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -1,5 +1,5 @@
 class Alumni < ActiveRecord::Base
   belongs_to :university
-  has_one :uni_major :through :university
+  has_one :uni_major, through: :university
   attr_accessible :age, :email, :graduation_year, :linkedin, :name, :phone, :university_id
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,5 +1,5 @@
 class University < ActiveRecord::Base
   has_many :uni_majors
-  has_many :majors :through :unimajors
+  has_many :majors, through: :unimajors
   attr_accessible :address, :alumni_id, :balance, :city, :email, :name, :phone, :state, :uni_major_id
 end

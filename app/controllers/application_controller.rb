3448407d4b2fc196d24c7a761fbe52740cdc1bec
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2014-10-01 17:56:18 -0400

    redirection removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  def admin
-    
-  end
-    #def after_sign_in_path_for(resource)
-      #if resource.role == "admin"
-         
-      #request.env['omniauth.origin'] || stored_location_for(resource) || admin_path
-      
-     # end
-    #if resource.role == "university" 
-     #request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-    #end
-   
-#   if rosource.role = "alumni" 
- #    request.env['omniauth.origin'] || stored_location_for(resource) || alumni_path
-  # end
-  #end
+
 end

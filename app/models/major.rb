8551070b3c0430ff93d039b6ba5eb5ee485d7253
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2014-10-14 13:42:05 -0400

    minor changes

--- app/models/major.rb
+++ app/models/major.rb
@@ -1,5 +1,4 @@
 class Major < ActiveRecord::Base
   belongs_to :uni_major
-  
   attr_accessible :description, :level, :name, :uni_major_id
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,7 +1,7 @@
 class University < ActiveRecord::Base
   has_many :uni_majors
-  has_many :majors, through: :unimajors
-  has_many :users
+  has_many :majors, through: :uni_majors
+  has_many :alumni
   attr_accessible :address, :alumni_id, :balance, :city, :email, :name, :phone, :state, :uni_major_id, :picture
   has_attached_file :picture, :default_url => "image1.jpg"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/

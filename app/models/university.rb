825f3fe921ef642c2bee457e3338d480533c3833
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2014-10-17 20:26:03 -0400

    modified university

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,7 +2,7 @@ class University < ActiveRecord::Base
   has_many :uni_majors
   has_many :majors, through: :uni_majors
   has_many :alumni
-  attr_accessible :address, :alumni_id, :balance, :city, :email, :name, :phone, :state, :uni_major_id, :picture, :majors
+  attr_accessible :address, :alumni_id, :balance, :city, :email, :name, :phone, :state, :uni_major_id, :picture, :majors, :major_ids
   has_attached_file :picture, :default_url => "image1.jpg"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   
